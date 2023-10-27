170d34f75a387e9b9b9cc303de5a07839820cc8f
Guilherme Santos Camilo <guilhermescamiloo@gmail.com>
2023-10-27 20:57:35 -0300

    Interface e Operação Adicionados
    1. Interface adicionada ao código.
    
    2. Operação da área sendo efetuada após clicar no botão "Calcular".
    
    3. Tratamento de erros efetuado.

--- AreaTrianguloForms/Form1.Designer.cs
+++ AreaTrianguloForms/Form1.Designer.cs
@@ -28,13 +28,93 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txbBase = new System.Windows.Forms.TextBox();
+            this.txbAltura = new System.Windows.Forms.TextBox();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.txbResultado = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txbBase
+            // 
+            this.txbBase.Location = new System.Drawing.Point(105, 111);
+            this.txbBase.Name = "txbBase";
+            this.txbBase.Size = new System.Drawing.Size(100, 20);
+            this.txbBase.TabIndex = 0;
+            // 
+            // txbAltura
+            // 
+            this.txbAltura.Location = new System.Drawing.Point(105, 153);
+            this.txbAltura.Name = "txbAltura";
+            this.txbAltura.Size = new System.Drawing.Size(100, 20);
+            this.txbAltura.TabIndex = 1;
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Location = new System.Drawing.Point(62, 114);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(37, 13);
+            this.lblBase.TabIndex = 2;
+            this.lblBase.Text = "Base :";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(62, 156);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(40, 13);
+            this.lblAltura.TabIndex = 3;
+            this.lblAltura.Text = "Altura :";
+            this.lblAltura.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(65, 193);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(115, 40);
+            this.btnCalcular.TabIndex = 4;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // txbResultado
+            // 
+            this.txbResultado.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txbResultado.Enabled = false;
+            this.txbResultado.Location = new System.Drawing.Point(86, 239);
+            this.txbResultado.Name = "txbResultado";
+            this.txbResultado.Size = new System.Drawing.Size(72, 13);
+            this.txbResultado.TabIndex = 5;
+            this.txbResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(252, 294);
+            this.Controls.Add(this.txbResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblBase);
+            this.Controls.Add(this.txbAltura);
+            this.Controls.Add(this.txbBase);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txbBase;
+        private System.Windows.Forms.TextBox txbAltura;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.TextBox txbResultado;
     }
 }
 
--- AreaTrianguloForms/Form1.cs
+++ AreaTrianguloForms/Form1.cs
@@ -16,5 +16,36 @@ namespace AreaTrianguloForms
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Declarar Variável:
+            double altura, basee;
+
+            try
+            {
+                //Declarar valor da variável:
+                altura = double.Parse(txbAltura.Text);
+                basee = double.Parse(txbBase.Text);
+
+                //Efetuar a operação:
+                txbResultado.Text = ((altura * basee) / 2).ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Dados Invalidos Informados!!!");
+                //Limpar txb's
+                txbBase.Clear();
+                txbAltura.Clear();
+                txbResultado.Clear();
+            }
+
+
+        }
     }
 }
